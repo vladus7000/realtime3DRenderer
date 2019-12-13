c9d772369f5a4fc7d275f460f2894dd08f910f9f
vladyslav.reviakin <vladyslav.reviakin@ubisoft.com>
2019-12-13 15:26:44 +0200

    first refactor pass 2

--- Core/Shader.hpp
+++ Core/Shader.hpp
@@ -1,29 +1,22 @@
 #pragma once
 
 #include <d3d11.h>
+#include <wrl.h>
 
 class Shader
 {
 public:
 	Shader() {}
-	Shader(ID3D11InputLayout* layout, ID3D11VertexShader* vs, ID3D11PixelShader* ps);
-	~Shader();
+	Shader(Microsoft::WRL::ComPtr <ID3D11InputLayout> layout
+		, Microsoft::WRL::ComPtr <ID3D11VertexShader> vs
+		, Microsoft::WRL::ComPtr <ID3D11PixelShader> ps);
 
-	Shader(const Shader& rhs);
-	Shader(Shader&& rhs);
-
-	Shader& operator=(const Shader& rhs);
-	Shader& operator=(Shader&& rhs);
-
-	ID3D11VertexShader* getVertexShader() const { return m_vertexShader; }
-	ID3D11PixelShader* getPixelShader() const { return m_pixelShader; }
-	ID3D11InputLayout* getInputLayout() const { return m_inputLayout; }
+	ID3D11VertexShader* getVertexShader() const { return m_vertexShader.Get(); }
+	ID3D11PixelShader* getPixelShader() const { return m_pixelShader.Get(); }
+	ID3D11InputLayout* getInputLayout() const { return m_inputLayout.Get(); }
 
 private:
-	void release();
-
-private:
-	ID3D11VertexShader* m_vertexShader = nullptr;
-	ID3D11PixelShader* m_pixelShader = nullptr;
-	ID3D11InputLayout* m_inputLayout = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader;
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
 };
